b059be3d5f02fd6096d53dab918fa22fbd87445c
Forjeon <forjeon22i7@gmail.com>
2024-10-10 10:21:21 -0600

    Finished Rocket hello world example; next up is continuing in Rocket guide to familiarize sufficiently, then doing the same for diesel (for mysql)

--- yaba/src/main.rs
+++ yaba/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+
+#[get("/")]
+fn index() -> &'static str {
+	"Hellow, wolrd!"
 }
+
+
+#[launch]
+fn rocket() -> _ {
+	rocket::build().mount("/", routes![index])
+}
+

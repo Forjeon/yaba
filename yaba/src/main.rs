34d127f41c111b03a5a5f837abb1d2fdbfddf520
Forjeon <forjeon22i7@gmail.com>
2024-10-11 19:19:16 -0600

    Got yaba DB connection working; next up is cleaning up / understanding said code, then testing connection via SELECTing in established routes

--- yaba/src/main.rs
+++ yaba/src/main.rs
@@ -1,14 +1,51 @@
 #[macro_use] extern crate rocket;
 
 
-#[get("/")]
-fn index() -> &'static str {
-	"Hellow, wolrd!"
+use rocket::{ Rocket, Build };
+use rocket::fairing::{ self, AdHoc };
+use rocket_db_pools::{ Database, Connection };
+use rocket_db_pools::diesel::{ prelude::*, MysqlPool, QueryResult };
+
+
+#[derive(Database)]
+#[database("yaba")]
+struct Db(MysqlPool);
+
+
+// GET requests
+#[get("/transaction")]
+fn get_trans() -> &'static str {
+	"TODO: get transactions"	// TODO
+}
+
+#[get("/category")]
+fn get_cat() -> &'static str {
+	"TODO: get categories"	// TODO
+}
+
+#[get("/account")]
+fn get_acc() -> &'static str {
+	"TODO: get accounts"	// TODO
 }
 
 
+// POST requests
+
+
+async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
+	if let Some(db) = Db::fetch(&rocket) {
+		Ok(rocket)
+	}
+	else {
+		Err(rocket)
+	}
+}
+
 #[launch]
 fn rocket() -> _ {
-	rocket::build().mount("/", routes![index])
+	rocket::build()
+		.attach(Db::init())
+		.attach(AdHoc::try_on_ignite("DB Migrations", run_migrations))
+		.mount("/", routes![get_trans, get_cat, get_acc])
 }
 
